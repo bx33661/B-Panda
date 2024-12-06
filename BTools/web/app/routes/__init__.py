a04167504752ab94ac70539a5185afbb42ef014f
bx33661 <bx33661@gmail.com>
2024-12-06 14:21:12 +0800

    更新

--- BTools/web/app/routes/__init__.py
+++
@@ -1,5 +0,0 @@
-from flask import Blueprint
-
-bp = Blueprint('routes', __name__)
-
-from . import pdf_routes, email_routes, find_routes,bs_routes
--- BTools/web/app/utils/__init__.py
+++
@@ -1,4 +0,0 @@
-from .pdf_utils import *
-from .email_utils import *
-from .find_utils import *
-from .bs_utils import *
